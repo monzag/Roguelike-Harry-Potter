02009b3cdf4aa1ce8c743fe66a5e3dbbe2c6ecbe
sylwia <zaleska.syl@gmail.com>
2017-05-08 12:52:25 +0200

    WASD working

--- rogue_simple_board.py
+++ rogue_simple_board.py
@@ -4,20 +4,19 @@ import os
 
 
 def create_board(width, height):
-	board = []
-	for row in range(0, height):
-		board_row = []
-		for column in range(0, width):
-			if row == 0 or row == height-1:
-				board_row.append("X")
-			else:
-				if column == 0 or column == width - 1:
-					board_row.append("X")
-				else:
-					board_row.append(" ")
-		board.append(board_row)
-
-	return board
+    board = []
+    for row in range(0,height):
+        board_row = []
+        for column in range(0, width):
+            if row == 0 or row == height-1:
+                board_row.append("X")
+            else:
+                if column == 0 or column == width - 1:
+                    board_row.append("X")
+                else:
+                    board_row.append(" ")
+        board.append(board_row)
+    return board
 
 
 def print_board(board):
@@ -27,8 +26,8 @@ def print_board(board):
         print()
 
 
-def insert_player(board, width, height):
-    board[height][width] = '@'
+def insert_player(board, x_pos, y_pos):
+    board[y_pos][x_pos] = '@'
     return board
 
 
@@ -44,33 +43,47 @@ def getch():
     return ch
 
 
-def user_move(x_pos, y_pos, character):
-    if character == 'a' and x_pos > 1:
+def user_move(x_pos, y_pos, character, board):
+    old_position_x = x_pos
+    old_position_y = y_pos
+    left_side_x = board[y_pos][x_pos - 1]
+    right_side_x = board[y_pos][x_pos + 1]
+    up_side_x = board[y_pos -1][x_pos]
+    down_side_x =board[y_pos + 1][x_pos]
+
+    if character == 'a' and left_side_x != "X":
         x_pos -= 1
-    elif character == 'd' and x_pos < 58:
+    elif character == 'd' and right_side_x!= "X":
         x_pos += 1
-    elif character == 'w' and y_pos > 1:
+    elif character == 'w' and up_side_x != "X":
         y_pos -= 1
-    elif character == 's' and y_pos < 18:
+    elif character == 's' and down_side_x != "X":
         y_pos += 1
-    elif character == "x":
-        exit()
+        #return x_pos, y_pos
+    else:
+        x_pos = old_position_x
+        y_pos = old_position_y
     return x_pos, y_pos
+
 
 
 def main():
     x_pos = 15
     y_pos = 5
+    height = 60
+    width = 20
     character = ' '
-    board = create_board(60, 20)
+    board = create_board(height, width)
     board_with_player = insert_player(board, x_pos, y_pos)
     print_board(board_with_player)
-    while character != 'X':
+    while character != 'x':
         character = getch()
-        x_pos, y_pos = user_move(x_pos, y_pos, character)
+        x_pos, y_pos = user_move(x_pos, y_pos, character, board)
+        print(x_pos)
         os.system("clear")
-        board = create_board(60, 20)
+        board = create_board(height, width)
         board_with_player = insert_player(board, x_pos, y_pos)
         print_board(board_with_player)
+    exit()
 
 main()
