a6a4aa414cc27218ddb2ac6d8091999d1c9537ec
monzag <zaganiacz.m@gmail.com>
2017-05-12 01:15:41 +0200

    4 level story

---
+++ story_forest.py
@@ -0,0 +1,30 @@
+print('''
+#####################################################################  
+#                                                                   #  
+#                                                                   #  
+#    Find your way in the maze of trees to the car.                 #  
+#                                                                   #  
+#    But be careful You-know-who is watching.                       #  
+#                                                                   #  
+#    In Forbidden forest Aragog is waiting for you                  #  
+#                                                                   #  
+#    I hope you are good in hot-cold game.                          #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#####################################################################
+''')
