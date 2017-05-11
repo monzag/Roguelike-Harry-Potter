e968275e9f0b31aba23ab66cb9e8785af4a6bdcf
monzag <zaganiacz.m@gmail.com>
2017-05-12 01:14:27 +0200

    1 level story

---
+++ story_alley.py
@@ -0,0 +1,30 @@
+print('''
+#####################################################################  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#     Now You are in Diagon Alley.                                  #  
+#     Before travel to Hogwart you must collect:                    #  
+#     * magic wand                                                  #  
+#     * quidditch broomstick                                        #  
+#     * books                                                       #  
+#     * owl                                                         #  
+#     * sweet beans                                                 #  
+#                                                                   #  
+#     Be carefuel becaus you-know-who is looking for you.           #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#####################################################################
+''')
