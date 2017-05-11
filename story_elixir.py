da9afdb6e0083bb33628fb2b224b844dc879be6b
monzag <zaganiacz.m@gmail.com>
2017-05-12 01:15:24 +0200

    3 level story

---
+++ story_elixir.py
@@ -0,0 +1,31 @@
+print('''
+#####################################################################  
+#                                                                   #  
+#    Focus!                                                         #  
+#    HOGWART is a school of witchcraft and wizardry students        #  
+#                                                                   # 
+#    The Headmaster of Hogwarts is Albus DUMBLEDORE                 #  
+#                                                                   #  
+#    Students are assigned to one of four homes:                    #  
+#    GRYFFINDOR, HUFFLEPUFF, RAVENCLAW and SLYTHERIN.               #  
+#                                                                   #  
+#    SNAPE, who teaches potions is a slytherin guardian.            #  
+#                                                                   #  
+#    At school you will find out that the ALOHOMORA                 #  
+#    is a spell that opens the locks.                               #  
+#                                                                   #  
+#    Harry POTTER has a SCAR in the shape of a lightning bolt.      #  
+#                                                                   #  
+#    His bestfriends are RON and HERMIONE.                          #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#                                                                   #  
+#####################################################################                                                                                                                                   
+''')
